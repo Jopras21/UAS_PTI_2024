bc1d00118a575b12df56fee89f2e70fe11eef7f6
Dominico <toni8ahia@gmail.com>
2024-05-12 19:41:46 +0700

    Sisa CSS dan API utk History

--- src/History/cardHistory.jsx
+++ src/History/cardHistory.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-function CardHistory(){
+function CardHistory(props){
     return(
         <div className="card">
-            <div>
-                
+            <div className="imgContainer">
+                <img src={props.img}/>
+            </div>
+            <div className="cardBody">
+                <h2>{props.name}</h2>
+                <p>{props.description}</p>
             </div>
             <hr />
         </div>
     );
 }
+export default CardHistory;
--- src/History/location.js
+++ src/History/location.js
@@ -1,14 +1,16 @@
-const location = [
+const Location = [
     {
         id: 1,
-        img: ,
+        img: "./assets/BgSejarah/gedunglondon.jpg",
         name: "Gedung London Sumatera",
-        description: 
+        description: "Dibangun pada tahun 1909 dan menjadi gedung kantor milik Harrisons & Crossfield Plc, sebuah perusahaan perkebunan dan perdagangan berbasis di London. Beberapa kali gedung ini sempat berpindah tangan dan sekarang dimiliki oleh PT PP London Sumatera. Sampai sekarang gedung ini masih digunakan sebagai gedung kantor untuk perusahaan agrikultur."
     },
     {
         id: 2,
-        img: ,
-        name: ,
-        description: 
+        img: "./assets/BgSejarah/istanamaimun.jpg",
+        name: "Istana Maimun",
+        description: "Peninggalan sejarah Kerajaan Deli yang sekarang telah diubah fungsinya menjadi sebuah Museum. Istana ini dibangun oleh Sultan Ma'amun Al-Rashid Perkasa Alamasyah yang memerintah Kerajaan ini antara 1873 - 1924. Pembangunan dimulai pada tahun 1888 dan selesai pada tahun 1891." 
     }
-]
+];
+
+export default Location;
