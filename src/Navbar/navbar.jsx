9d0fd64ef949acb0e64d1ab7ae11678cc41d40f6
Jopras21 <sun.phin098@gmail.com>
2024-05-13 18:07:26 +0700

    gw gatau ini apa

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const Navbar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const handleSearch = (e) => {
     const value = e.target.value;
     setSearchTerm(value);
@@ -15,19 +15,57 @@ const Navbar = ({ onSearch }) => {
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-15 w-10"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1]  shadow  w-52">
-            <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-            <li className="text-2xl"><NavLink to="/History" activeClassName="active">History</NavLink></li>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1]  shadow  w-52"
+          >
             <li>
-              <NavLink to="/Destination" activeClassName="active">Destination</NavLink>
+              <NavLink exact to="/" activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li className="text-2xl">
+              <NavLink to="/History" activeClassName="active">
+                History
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Destination" activeClassName="active">
+                Destination
+              </NavLink>
               <ul className="p-2">
-                <li><NavLink to="/Destination/Wisata" activeClassName="active">Wisata</NavLink></li>
-                <li><NavLink to="/Destination/Kuliner" activeClassName="active">Kuliner</NavLink></li>
+                <li>
+                  <NavLink to="/Destination/Wisata" activeClassName="active">
+                    Wisata
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/Destination/Kuliner" activeClassName="active">
+                    Kuliner
+                  </NavLink>
+                </li>
               </ul>
             </li>
-            <li><NavLink to="/About" activeClassName="active">About Us</NavLink></li>
+            <li>
+              <NavLink to="/About" activeClassName="active">
+                About Us
+              </NavLink>
+            </li>
           </ul>
         </div>
         <NavLink className="btn-ghost hidden lg:flex text-5xl" to="/">
@@ -36,24 +74,48 @@ const Navbar = ({ onSearch }) => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li className="text-2xl"><NavLink to="/" activeClassName="active" className="mx-20">Home</NavLink></li>
-          <li className="text-2xl"><NavLink to="/History" activeClassName="active">History</NavLink></li>
+          <li className="text-2xl">
+            <NavLink to="/" activeClassName="active" className="mx-20">
+              Home
+            </NavLink>
+          </li>
+          <li className="text-2xl">
+            <NavLink to="/History" activeClassName="active">
+              History
+            </NavLink>
+          </li>
           <li className="text-2xl">
             <details>
               <summary>Destination</summary>
               <ul className="p-2  w-44">
-                <li className="text-xl"><NavLink to="/Destination/Wisata" activeClassName="active">Wisata</NavLink></li>
-                <li className="text-xl"><NavLink to="/Destination/Kuliner" activeClassName="active">Kuliner</NavLink></li>
+                <li className="text-xl">
+                  <NavLink to="/Destination/Wisata" activeClassName="active">
+                    Wisata
+                  </NavLink>
+                </li>
+                <li className="text-xl">
+                  <NavLink to="/Destination/Kuliner" activeClassName="active">
+                    Kuliner
+                  </NavLink>
+                </li>
               </ul>
             </details>
           </li>
-          <li className="text-2xl"><NavLink to="/About" activeClassName="active">About Us</NavLink></li>
-          <li className="text-2xl"><NavLink to="/Feedback" activeClassName="active">Feedback</NavLink></li>
+          <li className="text-2xl">
+            <NavLink to="/About" activeClassName="active">
+              About Us
+            </NavLink>
+          </li>
+          <li className="text-2xl">
+            <NavLink to="/Feedback" activeClassName="active">
+              Feedback
+            </NavLink>
+          </li>
         </ul>
       </div>
       <div className="navbar-end hidden lg:flex">
         <div className="flex items-center">
-        <input
+          <input
             type="text"
             value={searchTerm}
             onChange={handleSearch}
