759f5ac32894b230d36a925d2b0a5825bfb522dd
JoeRscL <jonathan20strada@gmail.com>
2024-05-13 21:36:32 +0700

    tambahan history

--- src/History/descAwal.jsx
+++ src/History/descAwal.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 function DescAwal() {
   return (
-    <div>
+    
+    <div className="DescHis">
+      <div className="JudulHis">
+      <h1>Sejarah Singkat Provinsi Sumatera Utara</h1>
+      </div>
       <p>
         Saat zaman pemerintahan Belanda, Sumatera Utara merupakan pusat
         pemerintahan dari seluruh pulau Sumatera bernama Gouvernement van
         Sumatera dipimpin oleh seorang Gubernur di Kota Medan.
       </p>
+      <br />
       <p>
         Setelah kemerderkaan, dalam sidang pertama Komite Nasional Daerah,
         Provinsi Sumatera dibagi menjadi tiga Provinsi yakni Sumatera Utara,
@@ -15,6 +20,7 @@ function DescAwal() {
         merupakan penggabungan antara tiga daerah administratif yakni
         Keresidenan Aceh, Keresidenan Sumatera Timur, dan Keresidenan Tapanuli.
       </p>
+      <br />
       <p>
         Saat Undang-Undang Republik Indonesia No. 10 Tahun 1948 diterbitkan pada
         tanggal 15 April 1948, ditetapkan bahwa wilayah Sumatera dibagi menjadi
@@ -22,6 +28,7 @@ function DescAwal() {
         Sumatera Utara, Sumatera Tengah, Sumatera Selatan. Tanggal 15 April 1948
         ditetapkan menjadi hari jadi Provinsi Sumatera Utara.
       </p>
+      <br />
       <p>
         Pada awal tahun 1949 dilakukan reorganisasi kembali, dengan Keputusan
         Pemerintah Darurat RI Nomor 22/Pem/PDRI pada tanggal 17 Mei 1949,
@@ -31,11 +38,15 @@ function DescAwal() {
         Pengganti UU No. 5 Tahun 1950 pada tanggal 14 Agustus 1950, Ketetapan
         tersebut dicabut dan Provinsi Sumatera Utara dibentuk kembali.
       </p>
+      <br />
       <p>
         Dengan UU RI No. 24 Tahun 1956 yang ditetapkan pada tanggal 7 Desember
         1956, dibentuk Daerah Otonom Provinsi Aceh sehingga wilayah Provinsi
         Sumatera Utara terbagi menjadi dua.
       </p>
+      <div className="JudulHis">
+      <h1>Tempat Bersejarah</h1>
+      </div>
     </div>
   );
 }
