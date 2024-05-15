b3c403d01c3241cb17cb9b39bb2f1b5b6cf17131
Jopras21 <sun.phin098@gmail.com>
2024-05-15 12:39:15 +0700

    wow

--- src/History/cardHis.jsx
+++ src/History/cardHis.jsx
@@ -7,9 +7,7 @@ const Card = () => {
       <img viewBox="0 0 24 24" src='../src/assets/BgSejarah/arca.jpeg' />
       <div className="card__content">
         <p className="card__title">Card Title</p>
-        <p className="card__description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-        </p>
+        <p className="card__description">joe anak jomok pepeq kudanil</p>
       </div>
     </div>
   );
