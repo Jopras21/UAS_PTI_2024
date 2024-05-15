a6a0613ea8d1167670c85c3e87ace0961a66bb86
Dominico <toni8ahia@gmail.com>
2024-05-16 00:13:49 +0700

    Tambahin baju adat di history

--- src/History/history.jsx
+++ src/History/history.jsx
@@ -3,6 +3,7 @@ import Toba from "../assets/BgSejarah/toba.jpg";
 import DescAwal from "./descAwal";
 import CardHistory from "./cardHis.jsx";
 import Location from "./location.js";
+import Pakaian from "./pakaian.js";
 import Navbar from "../Navbar/navbar";
 import Footer from "../Footer/footer";
 import Lagu from "../Lagu/lagu.jsx";
@@ -34,6 +35,29 @@ function History() {
                 />
               );
             })}
+          </div>
+          <div>
+            <div className="JudulHis">
+              <h1>Pakaian Adat Sumatera Utara</h1>
+            </div>
+            <div className="DescHis">
+              <p>
+                Sumatera Utara kaya akan keberagaman budaya terutama dalam aspek budaya dan adat istiadat, salah satunya adalah pakaian adat.
+                Berikut merupakan beberapa pakaian adat yang ada di Sumatera Utara yang berasal dari berbagai suku yang berbeda
+              </p>
+            </div>
+            <br />
+            <div className="cardContainer">
+              {Pakaian.map(function (Pakaian) {
+                return (
+                  <CardHistory
+                    key={Pakaian.id}
+                    img={Pakaian.img}
+                    name={Pakaian.name}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
---
+++ src/History/pakaian.js
@@ -0,0 +1,35 @@
+import Karo from "../assets/Pakaian/karo.jpeg";
+import BatakToba from "../assets/Pakaian/bataktoba.jpeg";
+import Mandailing from "../assets/Pakaian/mandailing.jpeg";
+import PakPak from "../assets/Pakaian/pakpak.jpeg";
+import Nias from "../assets/Pakaian/nias.jpeg";
+
+const Pakaian = [
+    {
+        id: 1,
+        img: BatakToba,
+        name: "Pakaian Adat Batak Toba"
+    },
+    {
+        id: 2,
+        img: Karo,
+        name: "Pakaian Adat Karo"
+    },
+    {
+        id: 3,
+        img: Mandailing,
+        name: "Pakaian Adat Mandailing"
+    },
+    {
+        id: 4,
+        img: PakPak,
+        name: "Pakaian Adat Pakpak"
+    },
+    {
+        id: 5,
+        img: Nias,
+        name: "Pakaian Adat Nias"
+    }
+];
+
+export default Pakaian;
