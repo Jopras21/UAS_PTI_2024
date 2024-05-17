0c668d6e163ab2924838fb117ad2e8781118890b
Jopras21 <sun.phin098@gmail.com>
2024-05-17 17:40:14 +0700

    wow

--- src/Destination/destination.jsx
+++ src/Destination/destination.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar/navbar.jsx";
 import Carousel from "../Carousel/carousel.jsx";
 import Culinary from "./culinary.jsx";
@@ -8,71 +8,61 @@ import Video from "../assets/Video/destination.mp4";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import "leaflet/dist/leaflet.css";
-import { Icon, divIcon, point } from "leaflet";
+import { Icon } from "leaflet";
 import "./destination.css";
-import Lagu from "../Lagu/lagu.jsx";
 import TopButton from "../Button/topButton.jsx";
 
 function Destination() {
-  //   var map = L.map("map").setView([51.505, -0.09], 13);
-  //   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  //     maxZoom: 19,
-  //     attribution:
-  //       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  //   }).addTo(map);
+  const [selectedMarker, setSelectedMarker] = useState(null);
 
-  //   navigator.geolocation.watchPosition(succes, error);
-
-  //   function succes(pos) {
-  //     const lat = pos.coords. latitude;
-  //     const lng = pos.coords. longitude;
-  //     const accuracy = pos.coords.accuray;
-
-  //     let marker = L.marker([lat, lng]).addTo(map);
-  //     let circle = L.circle([lat, lng], {radius: accuracy}).addTo(map);
-
-  //     map.fitbounds(circle.getBounds());
-  //   }
-
-  //   function error(err) {
-  //     if (err.code === 1) {
-  //         alert("Please allow geolocation acces");
-  //     } else (
-  //         alert("Something went wrong")
-  //     )
-  //   }
   const markers = [
     {
       geocode: [2.7861, 98.6161],
+      img: "../src/assets/Landscape/Toba.png",
       popup: "🏞️ Danau Toba",
+      description: "Danau Toba adalah danau vulkanik terbesar di dunia.",
     },
     {
       geocode: [3.2, 98.5167],
+      img: "",
       popup: "⛰️ Gunung Sibayak",
+      description:
+        "Gunung Sibayak adalah gunung berapi aktif yang populer untuk hiking.",
     },
     {
       geocode: [3.5435, 98.1324],
-      popup: "🏔️ Bukit lawang (treking dan pusat orang utan)",
+      popup: "🏔️ Bukit Lawang (trekking dan pusat orangutan)",
+      description: "Bukit Lawang terkenal dengan pusat konservasi orangutan.",
     },
     {
       geocode: [2.9165, 98.516],
       popup: "💦 Air Terjun Sipiso Piso",
+      description:
+        "Air Terjun Sipiso Piso adalah salah satu air terjun tertinggi di Indonesia.",
     },
     {
       geocode: [3.1973, 98.541],
       popup: "🌳 Taman Alam Lumbini",
+      description:
+        "Taman Alam Lumbini adalah tempat suci Buddha dengan pagoda emas.",
     },
     {
       geocode: [0.8667, 104.4167],
       popup: "🏝️ Pulau Berhala",
+      description:
+        "Pulau Berhala adalah pulau eksotis dengan pantai yang indah.",
     },
     {
       geocode: [3.1696, 98.393],
       popup: "🏔️ Gunung Sinabung",
+      description:
+        "Gunung Sinabung adalah gunung berapi aktif yang sering meletus.",
     },
     {
       geocode: [2.9526, 99.0594],
       popup: "🦚 Taman Hewan Pematang Siantar",
+      description:
+        "Taman Hewan Pematang Siantar adalah kebun binatang yang terkenal.",
     },
   ];
 
@@ -80,14 +70,6 @@ function Destination() {
     iconUrl: Pointer,
     iconSize: [38, 38],
   });
-
-  // const createCustomClusterIcon = (cluster) => {
-  //   return new divIcon({
-  //     html: `<div class="cluster-icon>${cluster.getChildCount()}</div>`,
-  //     className: "custom-marker-cluster",
-  //     iconSize: point(33, 33, true),
-  //   });
-  // };
 
   return (
     <>
@@ -108,29 +90,39 @@ function Destination() {
               <source src={Video} type="video/mp4" />
             </video>
           </div>
-          <div id="map">
+          <div id="map" className="map-container">
             <MapContainer center={[2.1154, 99.5451]} zoom={7}>
               <TileLayer url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=ot1stQR4IlixqPHIopnI" />
-              <MarkerClusterGroup
-              // chunkedLoading
-              // iconCreateFunction={createCustomClusterIcon}
-              >
+              <MarkerClusterGroup>
                 {markers.map((marker) => (
-                  <Marker position={marker.geocode} icon={customIcon}>
+                  <Marker
+                    key={marker.popup}
+                    position={marker.geocode}
+                    icon={customIcon}
+                    eventHandlers={{
+                      click: () => {
+                        setSelectedMarker(marker);
+                      },
+                    }}
+                  >
                     <Popup>{marker.popup}</Popup>
                   </Marker>
                 ))}
               </MarkerClusterGroup>
             </MapContainer>
+            {selectedMarker && (
+              <div className="marker-description">
+                <img src={selectedMarker.img} alt={selectedMarker.popup} />
+                <h2>{selectedMarker.popup}</h2>
+                <p>{selectedMarker.description}</p>
+              </div>
+            )}
           </div>
           <div className="persue"></div>
         </div>
         <div className="culinary">
           <Culinary />
         </div>
-        {/* <div className="lagu">
-          <Lagu query="Sinanggar Tulo" />
-        </div> */}
       </div>
       <div className="footer">
         <Footer />
