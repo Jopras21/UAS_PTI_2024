b334944b993afff992a7e70166eca2e3ebe5a9f0
Dominico <toni8ahia@gmail.com>
2024-05-19 14:01:37 +0700

    Tambahin lightbox ke gallery history

--- src/Gallery/gallery.jsx
+++ src/Gallery/gallery.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
 import PhotoAlbum from "react-photo-album";
+
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+//plugins buat lightbox
+import { Captions, Fullscreen, Thumbnails } from "yet-another-react-lightbox/plugins";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 const photos = [
   {
@@ -9,6 +19,8 @@ const photos = [
       { src: "../src/assets/Pakaian/bataktoba.jpeg", width: 240, height: 180 },
       { src: "../src/assets/Pakaian/bataktoba.jpeg", width: 120, height: 90 },
     ],
+    title: "Pakaian Adat Batak Toba",
+    description: "Source: detik.com/sumut"
   },
   {
     src: "../src/assets/Pakaian/karo.jpeg",
@@ -18,6 +30,8 @@ const photos = [
       { src: "../src/assets/Pakaian/karo.jpeg", width: 240, height: 180 },
       { src: "../src/assets/Pakaian/karo.jpeg", width: 120, height: 90 },
     ],
+    title: "Pakaian Adat Karo",
+    description: "Source: detik.com/sumut"
   },
   {
     src: "../src/assets/Pakaian/angkola2.jpeg",
@@ -27,6 +41,8 @@ const photos = [
       { src: "../src/assets/Pakaian/angkola2.jpeg", width: 240, height: 180 },
       { src: "../src/assets/Pakaian/angkola2.jpeg", width: 120, height: 90 },
     ],
+    title: "Pakaian Adat Angkola",
+    description: "Source: detik.com/sumut"
   },
   {
     src: "../src/assets/Pakaian/nias.jpeg",
@@ -36,6 +52,8 @@ const photos = [
       { src: "../src/assets/Pakaian/nias.jpeg", width: 240, height: 180 },
       { src: "../src/assets/Pakaian/nias.jpeg", width: 120, height: 90 },
     ],
+    title: "Pakaian Adat Nias",
+    description: "Source: detik.com/sumut"
   },
   {
     src: "../src/assets/Pakaian/pakpak.jpeg",
@@ -45,15 +63,19 @@ const photos = [
       { src: "../src/assets/Pakaian/pakpak.jpeg", width: 240, height: 180 },
       { src: "../src/assets/Pakaian/pakpak.jpeg", width: 120, height: 90 },
     ],
+    title: "Pakaian Adat Pakpak",
+    description: "Source: detik.com/sumut"
   },
   {
     src: "../src/assets/Pakaian/angkola.jpeg",
     width: 480,
     height: 360,
     srcSet: [
-      { src: "../src/assets/Pakaian/angkola2.jpeg", width: 240, height: 180 },
-      { src: "../src/assets/Pakaian/angkola2.jpeg", width: 120, height: 90 },
+      { src: "../src/assets/Pakaian/angkola.jpeg", width: 240, height: 180 },
+      { src: "../src/assets/Pakaian/angkola.jpeg", width: 120, height: 90 },
     ],
+    title: "Pakaian Adat Angkola",
+    description: "Source: detik.com/sumut"
   },
   {
     src: "../src/assets/Pakaian/melayu.jpg",
@@ -63,6 +85,8 @@ const photos = [
       { src: "../src/assets/Pakaian/melayu.jpg", width: 172, height: 256 },
       { src: "../src/assets/Pakaian/melayu.jpg", width: 86, height: 128 },
     ],
+    title: "Pakaian Adat Melayu",
+    description: "Source: gramedia.com"
   },
   {
     src: "../src/assets/Pakaian/sibolga.jpg",
@@ -72,6 +96,8 @@ const photos = [
       { src: "../src/assets/Pakaian/sibolga.jpg", width: 200, height: 250 },
       { src: "../src/assets/Pakaian/sibolga.jpg", width: 100, height: 125 },
     ],
+    title: "Pakaian Adat Sibolga",
+    description: "Source: orami.co.id"
   },
   {
     src: "../src/assets/Pakaian/karo2.jpg",
@@ -81,6 +107,8 @@ const photos = [
       { src: "../src/assets/Pakaian/karo2.jpg", width: 161, height: 256 },
       { src: "../src/assets/Pakaian/karo2.jpg", width: 81, height: 128 },
     ],
+    title: "Pakaian Adat Karo",
+    description: "Source: pinterest.com"
   },
   {
     src: "../src/assets/Pakaian/simalungun.jpg",
@@ -90,6 +118,8 @@ const photos = [
       { src: "../src/assets/Pakaian/simalungun.jpg", width: 300, height: 375 },
       { src: "../src/assets/Pakaian/simalungun.jpg", width: 150, height: 188 },
     ],
+    title: "Pakaian Adat Simalungun",
+    description: "Source: rukita.co"
   },
   {
     src: "../src/assets/Pakaian/mandailing.jpg",
@@ -99,6 +129,8 @@ const photos = [
       { src: "../src/assets/Pakaian/mandailing.jpg", width: 400, height: 508 },
       { src: "../src/assets/Pakaian/mandailing.jpg", width: 266, height: 339 },
     ],
+    title: "Pakaian Adat Mandailing",
+    description: "Source: rimbakita.com"
   },
   {
     src: "../src/assets/Pakaian/nias2.png",
@@ -108,9 +140,27 @@ const photos = [
       { src: "../src/assets/Pakaian/nias2.png", width: 472, height: 340 },
       { src: "../src/assets/Pakaian/nias2.png", width: 236, height: 170 },
     ],
+    title: "Pakaian Adat Nias",
+    description: "Source: wartanias.com"
   },
 ];
 
 export default function Gallery() {
-  return <PhotoAlbum layout="rows" photos={photos} />;
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <>
+      <PhotoAlbum layout="rows" photos={photos} onClick={({ index }) => setIndex(index)} />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Captions, Thumbnails]}
+        captions={{showToggle: true}}
+      />
+    </>
+  );
 }
