a3148bb2c0f6b0a5e5eca16d1984b6da0c5d1772
hexselarchieles <hexselavmm@gmail.com>
2024-05-20 09:44:09 +0700

    update galery

--- src/Gallery/gallery.jsx
+++ src/Gallery/gallery.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PhotoAlbum from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
@@ -13,135 +13,148 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "./gallery.css";
 import { motion, useAnimation } from "framer-motion";
 
+import BatakToba from "../assets/Pakaian/bataktoba.jpeg";
+import Karo from "../assets/Pakaian/karo.jpeg";
+import Angkola2 from "../assets/Pakaian/angkola2.jpeg";
+import Nias from "../assets/Pakaian/nias.jpeg";
+import Pakpak from "../assets/Pakaian/pakpak.jpeg";
+import Angkola from "../assets/Pakaian/angkola.jpeg";
+import Melayu from "../assets/Pakaian/melayu.jpg";
+import Sibolga from "../assets/Pakaian/sibolga.jpg";
+import Karo2 from "../assets/Pakaian/karo2.jpg";
+import Simalungun from "../assets/Pakaian/simalungun.jpg";
+import Mandailing from "../assets/Pakaian/mandailing.jpg";
+import Nias2 from "../assets/Pakaian/nias2.png";
+
 const photos = [
   {
-    src: "../src/assets/Pakaian/bataktoba.jpeg",
+    src: BatakToba,
     width: 480,
     height: 360,
     srcSet: [
-      { src: "../src/assets/Pakaian/bataktoba.jpeg", width: 240, height: 180 },
-      { src: "../src/assets/Pakaian/bataktoba.jpeg", width: 120, height: 90 },
+      { src: BatakToba, width: 240, height: 180 },
+      { src: BatakToba, width: 120, height: 90 },
     ],
     title: "Pakaian Adat Batak Toba",
     description: "Source: detik.com/sumut",
   },
   {
-    src: "../src/assets/Pakaian/karo.jpeg",
+    src: Karo,
     width: 480,
     height: 360,
     srcSet: [
-      { src: "../src/assets/Pakaian/karo.jpeg", width: 240, height: 180 },
-      { src: "../src/assets/Pakaian/karo.jpeg", width: 120, height: 90 },
+      { src: Karo, width: 240, height: 180 },
+      { src: Karo, width: 120, height: 90 },
     ],
     title: "Pakaian Adat Karo",
     description: "Source: detik.com/sumut",
   },
   {
-    src: "../src/assets/Pakaian/angkola2.jpeg",
+    src: Angkola2,
     width: 480,
     height: 360,
     srcSet: [
-      { src: "../src/assets/Pakaian/angkola2.jpeg", width: 240, height: 180 },
-      { src: "../src/assets/Pakaian/angkola2.jpeg", width: 120, height: 90 },
+      { src: Angkola2, width: 240, height: 180 },
+      { src: Angkola2, width: 120, height: 90 },
     ],
     title: "Pakaian Adat Angkola",
     description: "Source: detik.com/sumut",
   },
   {
-    src: "../src/assets/Pakaian/nias.jpeg",
+    src: Nias,
     width: 480,
     height: 360,
     srcSet: [
-      { src: "../src/assets/Pakaian/nias.jpeg", width: 240, height: 180 },
-      { src: "../src/assets/Pakaian/nias.jpeg", width: 120, height: 90 },
+      { src: Nias, width: 240, height: 180 },
+      { src: Nias, width: 120, height: 90 },
     ],
     title: "Pakaian Adat Nias",
     description: "Source: detik.com/sumut",
   },
   {
-    src: "../src/assets/Pakaian/pakpak.jpeg",
+    src: Pakpak,
     width: 480,
     height: 360,
     srcSet: [
-      { src: "../src/assets/Pakaian/pakpak.jpeg", width: 240, height: 180 },
-      { src: "../src/assets/Pakaian/pakpak.jpeg", width: 120, height: 90 },
+      { src: Pakpak, width: 240, height: 180 },
+      { src: Pakpak, width: 120, height: 90 },
     ],
     title: "Pakaian Adat Pakpak",
     description: "Source: detik.com/sumut",
   },
   {
-    src: "../src/assets/Pakaian/angkola.jpeg",
+    src: Angkola,
     width: 480,
     height: 360,
     srcSet: [
-      { src: "../src/assets/Pakaian/angkola.jpeg", width: 240, height: 180 },
-      { src: "../src/assets/Pakaian/angkola.jpeg", width: 120, height: 90 },
+      { src: Angkola, width: 240, height: 180 },
+      { src: Angkola, width: 120, height: 90 },
     ],
     title: "Pakaian Adat Angkola",
     description: "Source: detik.com/sumut",
   },
   {
-    src: "../src/assets/Pakaian/melayu.jpg",
+    src: Melayu,
     width: 343,
     height: 512,
     srcSet: [
-      { src: "../src/assets/Pakaian/melayu.jpg", width: 172, height: 256 },
-      { src: "../src/assets/Pakaian/melayu.jpg", width: 86, height: 128 },
+      { src: Melayu, width: 172, height: 256 },
+      { src: Melayu, width: 86, height: 128 },
     ],
     title: "Pakaian Adat Melayu",
     description: "Source: gramedia.com",
   },
   {
-    src: "../src/assets/Pakaian/sibolga.jpg",
+    src: Sibolga,
     width: 400,
     height: 500,
     srcSet: [
-      { src: "../src/assets/Pakaian/sibolga.jpg", width: 200, height: 250 },
-      { src: "../src/assets/Pakaian/sibolga.jpg", width: 100, height: 125 },
+      { src: Sibolga, width: 200, height: 250 },
+      { src: Sibolga, width: 100, height: 125 },
     ],
     title: "Pakaian Adat Sibolga",
     description: "Source: orami.co.id",
   },
   {
-    src: "../src/assets/Pakaian/karo2.jpg",
+    src: Karo2,
     width: 322,
     height: 512,
     srcSet: [
-      { src: "../src/assets/Pakaian/karo2.jpg", width: 161, height: 256 },
-      { src: "../src/assets/Pakaian/karo2.jpg", width: 81, height: 128 },
+      { src: Karo2, width: 161, height: 256 },
+      { src: Karo2, width: 81, height: 128 },
     ],
     title: "Pakaian Adat Karo",
     description: "Source: pinterest.com",
   },
   {
-    src: "../src/assets/Pakaian/simalungun.jpg",
+    src: Simalungun,
     width: 600,
     height: 749,
     srcSet: [
-      { src: "../src/assets/Pakaian/simalungun.jpg", width: 300, height: 375 },
-      { src: "../src/assets/Pakaian/simalungun.jpg", width: 150, height: 188 },
+      { src: Simalungun, width: 300, height: 375 },
+      { src: Simalungun, width: 150, height: 188 },
     ],
     title: "Pakaian Adat Simalungun",
     description: "Source: rukita.co",
   },
   {
-    src: "../src/assets/Pakaian/mandailing.jpg",
+    src: Mandailing,
     width: 600,
     height: 763,
     srcSet: [
-      { src: "../src/assets/Pakaian/mandailing.jpg", width: 400, height: 508 },
-      { src: "../src/assets/Pakaian/mandailing.jpg", width: 266, height: 339 },
+      { src: Mandailing, width: 400, height: 508 },
+      { src: Mandailing, width: 266, height: 339 },
     ],
     title: "Pakaian Adat Mandailing",
     description: "Source: rimbakita.com",
   },
   {
-    src: "../src/assets/Pakaian/nias2.png",
+    src: Nias2,
     width: 944,
     height: 680,
     srcSet: [
-      { src: "../src/assets/Pakaian/nias2.png", width: 472, height: 340 },
-      { src: "../src/assets/Pakaian/nias2.png", width: 236, height: 170 },
+      { src: Nias2, width: 472, height: 340 },
+      { src: Nias2, width: 236, height: 170 },
     ],
     title: "Pakaian Adat Nias",
     description: "Source: wartanias.com",
@@ -163,7 +176,7 @@ export default function Gallery() {
             transition: { duration: 0.5 },
           });
         } else {
-          controls.start({ opacity: 0, y: 50 });
+          controls.start({ opacity: 0, y:50 });
         }
       },
       { threshold: 0.1 }
